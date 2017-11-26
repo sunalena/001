e87b9155a41c35d6831579b09b27266c18dadd63
sunalena <33164590+sunalena@users.noreply.github.com>
2017-11-26 16:57:42 +0300

    Update Day.js

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,31 +1,31 @@
 import React from 'react'
 
 const Lesson = ({ time, discipline, classroom, teacher }) => (
-  <tr>
-    <td>{time}</td>
-    <td>{discipline}</td>
-    <td>{classroom}</td>
-    <td>{teacher}</td>
-  </tr>
+    <tr>
+        <td>{time}</td>
+        <td>{discipline}</td>
+        <td>{classroom}</td>
+        <td>{teacher}</td>
+    </tr>
 )
 
 const itemToLesson = ({ id, ...rest }) => <Lesson key={id} {...rest} />
 
 export default ({ id, day, lessons }) => (
-  <table border="1" className=''>
-    <caption>{day}</caption>
-    <tbody>
-      <tr>
-        <th>Время</th>
-        <th>
-          Название <br /> пары
-        </th>
-        <th>
-          № <br />кабинета
-        </th>
-        <th>Преподаватель</th>
-      </tr>
-      {lessons.map(itemToLesson)}
-    </tbody>
-  </table>
+    <table border="1">
+      <caption><h2> {day}</h2></caption>
+          <tbody>
+              <tr>
+                  <th>Время</th>
+                  <th>
+                   Название <br /> пары
+                  </th>
+                  <th>
+                   № <br />кабинета
+                  </th>
+                  <th>Преподаватель</th>
+              </tr>
+                {lessons.map(itemToLesson)}
+          </tbody>
+    </table>
 )
