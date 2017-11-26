342dd28a5bdfafdfbb96668b4fd8267eb7ad8bea
sunalena <33164590+sunalena@users.noreply.github.com>
2017-11-26 16:59:14 +0300

    Update week.js

--- src/data/week.js
+++ src/data/week.js
@@ -165,6 +165,88 @@ const week = [
             teacher: ''
           }
         ]
+      },
+      {
+        id: 5,
+        day: 'Пятница',
+        lessons: [
+          {
+            id: 1,
+            time: '8.00-9.30',
+            discipline: 'Алгебра',
+            classroom: '325',
+            teacher: 'Ермакова Г.Н.'
+          },
+          {
+            id: 2,
+            time: '9.40-11.10',
+            discipline: 'Геометрия',
+            classroom: '323',
+            teacher: ''
+          },
+          {
+            id: 3,
+            time: '11.30-13.00',
+            discipline: 'Физкультура',
+            classroom: '---',
+            teacher: ''
+          },
+          {
+            id: 4,
+            time: '13.10-14.40',
+            discipline: '5',
+            classroom: '38',
+            teacher: '24'
+          },
+          {
+            id: 5,
+            time: '14.50-16.20',
+            discipline: '5,5',
+            classroom: '38',
+            teacher: ''
+          }
+        ]
+      },
+      {
+        id: 4,
+        day: 'Суббота',
+        lessons: [
+          {
+            id: 1,
+            time: '8.00-9.30',
+            discipline: 'Алгебра',
+            classroom: '325',
+            teacher: 'Ермакова Г.Н.'
+          },
+          {
+            id: 2,
+            time: '9.40-11.10',
+            discipline: 'Геометрия',
+            classroom: '323',
+            teacher: ''
+          },
+          {
+            id: 3,
+            time: '11.30-13.00',
+            discipline: 'Физкультура',
+            classroom: '---',
+            teacher: ''
+          },
+          {
+            id: 4,
+            time: '13.10-14.40',
+            discipline: '5',
+            classroom: '38',
+            teacher: '24'
+          },
+          {
+            id: 5,
+            time: '14.50-16.20',
+            discipline: '5,5',
+            classroom: '38',
+            teacher: ''
+          }
+        ]
       }
     ]
   },
@@ -178,30 +260,37 @@ const week = [
           {
             id: 1,
             time: '8.00-9.30',
-            discipline: 'Программирование',
-            classroom: 'ВЦ',
-            teacher: 'Калинкова Е.В.'
+            discipline: 'Алгебра',
+            classroom: '325',
+            teacher: 'Ермакова Г.Н.'
           },
           {
             id: 2,
             time: '9.40-11.10',
-            discipline: 'Программирование',
-            classroom: 'ВЦ',
-            teacher: 'Калинкова Е.В.'
+            discipline: 'Геометрия',
+            classroom: '323',
+            teacher: ''
           },
           {
             id: 3,
             time: '11.30-13.00',
-            discipline: 'Математический анализ',
-            classroom: '318',
-            teacher: 'Афонин В.В.'
+            discipline: 'Геометрия',
+            classroom: '---',
+            teacher: ''
           },
           {
             id: 4,
             time: '13.10-14.40',
-            discipline: 'Основы информатики',
-            classroom: 'ВЦ',
-            teacher: 'Дорохова Н.А.'
+            discipline: '5',
+            classroom: '38',
+            teacher: '24'
+          },
+          {
+            id: 5,
+            time: '14.50-16.20',
+            discipline: '5,5',
+            classroom: '38',
+            teacher: ''
           }
         ]
       },
@@ -213,29 +302,36 @@ const week = [
             id: 1,
             time: '8.00-9.30',
             discipline: 'Алгебра',
-            classroom: '313',
+            classroom: '325',
             teacher: 'Ермакова Г.Н.'
           },
           {
             id: 2,
             time: '9.40-11.10',
-            discipline: 'История',
-            classroom: '306 Г',
+            discipline: 'Геометрия',
+            classroom: '323',
             teacher: ''
           },
           {
             id: 3,
             time: '11.30-13.00',
-            discipline: 'Алгебра и геометрия',
-            classroom: '313',
-            teacher: 'Ермакова Г.Н.'
+            discipline: 'Физкультура',
+            classroom: '---',
+            teacher: ''
           },
           {
             id: 4,
             time: '13.10-14.40',
-            discipline: 'Основы информатики',
-            classroom: 'ВЦ',
-            teacher: 'Дорохова Н.А.'
+            discipline: '5',
+            classroom: '38',
+            teacher: '24'
+          },
+          {
+            id: 5,
+            time: '14.50-16.20',
+            discipline: '5,5',
+            classroom: '38',
+            teacher: ''
           }
         ]
       },
@@ -246,23 +342,37 @@ const week = [
           {
             id: 1,
             time: '8.00-9.30',
-            discipline: 'Физика',
-            classroom: '218',
-            teacher: 'Соковнич С.М.'
+            discipline: 'Алгебра',
+            classroom: '325',
+            teacher: 'Ермакова Г.Н.'
           },
           {
             id: 2,
             time: '9.40-11.10',
-            discipline: 'Физическая культура',
-            classroom: 'стадион',
+            discipline: 'Геометрия',
+            classroom: '323',
             teacher: ''
           },
           {
             id: 3,
             time: '11.30-13.00',
-            discipline: 'Языки программирования',
-            classroom: 'Марков Д.А.',
-            teacher: 'ВЦ'
+            discipline: 'Физкультура',
+            classroom: '---',
+            teacher: ''
+          },
+          {
+            id: 4,
+            time: '13.10-14.40',
+            discipline: '5',
+            classroom: '38',
+            teacher: '24'
+          },
+          {
+            id: 5,
+            time: '14.50-16.20',
+            discipline: '5,5',
+            classroom: '38',
+            teacher: ''
           }
         ]
       },
@@ -273,35 +383,361 @@ const week = [
           {
             id: 1,
             time: '8.00-9.30',
-            discipline: 'Физическая культура',
-            classroom: 'стадион',
-            teacher: ''
+            discipline: 'Алгебра',
+            classroom: '325',
+            teacher: 'Ермакова Г.Н.'
           },
           {
             id: 2,
             time: '9.40-11.10',
-            discipline: 'Основы ЭВМ',
-            classroom: 'ВЦ',
-            teacher: 'Бугаенко А.В.'
+            discipline: 'Геометрия',
+            classroom: '323',
+            teacher: ''
           },
           {
             id: 3,
             time: '11.30-13.00',
-            discipline: 'Основы ЭВМ',
-            classroom: 'ВЦ',
-            teacher: 'Бугаенко А.В.'
+            discipline: 'Физкультура',
+            classroom: '---',
+            teacher: ''
           },
           {
             id: 4,
             time: '13.10-14.40',
-            discipline: 'Языки программирования',
-            classroom: 'Марков Д.А.',
-            teacher: 'ВЦ'
+            discipline: '5',
+            classroom: '38',
+            teacher: '24'
+          },
+          {
+            id: 5,
+            time: '14.50-16.20',
+            discipline: '5,5',
+            classroom: '38',
+            teacher: ''
+          }
+        ]
+      },
+      {
+        id: 5,
+        day: 'Пятница',
+        lessons: [
+          {
+            id: 1,
+            time: '8.00-9.30',
+            discipline: 'Алгебра',
+            classroom: '325',
+            teacher: 'Ермакова Г.Н.'
+          },
+          {
+            id: 2,
+            time: '9.40-11.10',
+            discipline: 'Геометрия',
+            classroom: '323',
+            teacher: ''
+          },
+          {
+            id: 3,
+            time: '11.30-13.00',
+            discipline: 'Физкультура',
+            classroom: '---',
+            teacher: ''
+          },
+          {
+            id: 4,
+            time: '13.10-14.40',
+            discipline: '5',
+            classroom: '38',
+            teacher: '24'
+          },
+          {
+            id: 5,
+            time: '14.50-16.20',
+            discipline: '5,5',
+            classroom: '38',
+            teacher: ''
+          }
+        ]
+      },
+      {
+        id: 4,
+        day: 'Суббота',
+        lessons: [
+          {
+            id: 1,
+            time: '8.00-9.30',
+            discipline: 'Алгебра',
+            classroom: '325',
+            teacher: 'Ермакова Г.Н.'
+          },
+          {
+            id: 2,
+            time: '9.40-11.10',
+            discipline: 'Геометрия',
+            classroom: '323',
+            teacher: ''
+          },
+          {
+            id: 3,
+            time: '11.30-13.00',
+            discipline: 'Физкультура',
+            classroom: '---',
+            teacher: ''
+          },
+          {
+            id: 4,
+            time: '13.10-14.40',
+            discipline: '5',
+            classroom: '38',
+            teacher: '24'
+          },
+          {
+            id: 5,
+            time: '14.50-16.20',
+            discipline: '5,5',
+            classroom: '38',
+            teacher: ''
           }
         ]
       }
     ]
-  }
+  },
+  {
+  groupId: 42,
+  days: [
+    {
+      id: 1,
+      day: 'Понедельник',
+      lessons: [
+        {
+          id: 1,
+          time: '8.00-9.30',
+          discipline: 'Алгебра',
+          classroom: '325',
+          teacher: 'Ермакова Г.Н.'
+        },
+        {
+          id: 2,
+          time: '9.40-11.10',
+          discipline: 'Геометрия',
+          classroom: '323',
+          teacher: ''
+        },
+        {
+          id: 3,
+          time: '11.30-13.00',
+          discipline: 'Геометрия',
+          classroom: '---',
+          teacher: ''
+        },
+        {
+          id: 4,
+          time: '13.10-14.40',
+          discipline: '5',
+          classroom: '38',
+          teacher: '24'
+        }
+      ]
+    },
+    {
+      id: 2,
+      day: 'Вторник',
+      lessons: [
+        {
+          id: 1,
+          time: '8.00-9.30',
+          discipline: 'Алгебра',
+          classroom: '325',
+          teacher: 'Ермакова Г.Н.'
+        },
+        {
+          id: 2,
+          time: '9.40-11.10',
+          discipline: 'Геометрия',
+          classroom: '323',
+          teacher: ''
+        },
+        {
+          id: 3,
+          time: '11.30-13.00',
+          discipline: 'Физкультура',
+          classroom: '---',
+          teacher: ''
+        },
+        {
+          id: 4,
+          time: '13.10-14.40',
+          discipline: '5',
+          classroom: '38',
+          teacher: '24'
+        },
+        {
+          id: 5,
+          time: '14.50-16.20',
+          discipline: '5,5',
+          classroom: '38',
+          teacher: ''
+        }
+      ]
+    },
+    {
+      id: 3,
+      day: 'Среда',
+      lessons: [
+        {
+          id: 1,
+          time: '8.00-9.30',
+          discipline: 'Алгебра',
+          classroom: '325',
+          teacher: 'Ермакова Г.Н.'
+        },
+        {
+          id: 2,
+          time: '9.40-11.10',
+          discipline: 'Геометрия',
+          classroom: '323',
+          teacher: ''
+        },
+        {
+          id: 3,
+          time: '11.30-13.00',
+          discipline: 'Физкультура',
+          classroom: '---',
+          teacher: ''
+        },
+        {
+          id: 4,
+          time: '13.10-14.40',
+          discipline: '5',
+          classroom: '38',
+          teacher: '24'
+        },
+        {
+          id: 5,
+          time: '14.50-16.20',
+          discipline: '5,5',
+          classroom: '38',
+          teacher: ''
+        }
+      ]
+    },
+    {
+      id: 4,
+      day: 'Четверг',
+      lessons: [
+        {
+          id: 1,
+          time: '8.00-9.30',
+          discipline: 'Алгебра',
+          classroom: '325',
+          teacher: 'Ермакова Г.Н.'
+        },
+        {
+          id: 2,
+          time: '9.40-11.10',
+          discipline: 'Геометрия',
+          classroom: '323',
+          teacher: ''
+        },
+        {
+          id: 3,
+          time: '11.30-13.00',
+          discipline: 'Физкультура',
+          classroom: '---',
+          teacher: ''
+        },
+        {
+          id: 4,
+          time: '13.10-14.40',
+          discipline: '5',
+          classroom: '38',
+          teacher: '24'
+        },
+        {
+          id: 5,
+          time: '14.50-16.20',
+          discipline: '5,5',
+          classroom: '38',
+          teacher: ''
+        }
+      ]
+    },
+    {
+      id: 5,
+      day: 'Пятница',
+      lessons: [
+        {
+          id: 1,
+          time: '8.00-9.30',
+          discipline: 'Алгебра',
+          classroom: '325',
+          teacher: 'Ермакова Г.Н.'
+        },
+        {
+          id: 2,
+          time: '9.40-11.10',
+          discipline: 'Геометрия',
+          classroom: '323',
+          teacher: ''
+        },
+        {
+          id: 3,
+          time: '11.30-13.00',
+          discipline: 'Физкультура',
+          classroom: '---',
+          teacher: ''
+        },
+        {
+          id: 4,
+          time: '13.10-14.40',
+          discipline: '5',
+          classroom: '38',
+          teacher: '24'
+        },
+        {
+          id: 5,
+          time: '14.50-16.20',
+          discipline: '5,5',
+          classroom: '38',
+          teacher: ''
+        }
+      ]
+    },
+    {
+      id: 4,
+      day: 'Суббота',
+      lessons: [
+        {
+          id: 1,
+          time: '8.00-9.30',
+          discipline: 'Алгебра',
+          classroom: '325',
+          teacher: 'Ермакова Г.Н.'
+        },
+        {
+          id: 2,
+          time: '9.40-11.10',
+          discipline: 'Геометрия',
+          classroom: '323',
+          teacher: ''
+        },
+        {
+          id: 3,
+          time: '11.30-13.00',
+          discipline: 'Физкультура',
+          classroom: '---',
+          teacher: ''
+        },
+        {
+          id: 4,
+          time: '13.10-14.40',
+          discipline: '5',
+          classroom: '38',
+          teacher: '24'
+        }
+      ]
+    }
+  ]
+}
 ]
 
 export default week
