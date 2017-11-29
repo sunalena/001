38f196c09c3f4c3ca41e2bd19ada56d0f0ec3090
karapetyansa <karapetyan.sa@gmail.com>
2017-11-29 22:21:29 +0200

    server (#11)

--- src/components/Branch.js
+++ src/components/Branch.js
@@ -1,41 +1,37 @@
 import React, { PureComponent } from 'react'
-import hierarchy from '../data/hierarchy'
+
+const uri = window.location.origin
 
 export default class Branch extends PureComponent {
   state = {
-    current: undefined
+    children: [],
+    isOpen: false
   }
 
-  handleClick = ({ target }) => {
-    const current =
-      this.state.current === Number(target.id) ? undefined : Number(target.id)
-    this.setState({ current })
-    if (target.type === 'group') {
-      this.props.getTimeTable(Number(target.id))
+  handleClick = async ({ target }) => {
+    if (this.props.type === 'group') {
+      this.props.getTimeTable(Number(this.props.id))
+      return
     }
+    if (!this.state.isOpen) {
+      const URL = `${uri}/groups?parent=${this.props.id}`
+      const res = await fetch(URL)
+      this.setState({ children: await res.json(), isOpen: true })
+    } else this.setState({ children: [], isOpen: false })
   }
-
   elToLink = ({ ...props }) => (
-    <Branch
-      key={props.id}
-      isOpen={props.id === this.state.current}
-      onClick={this.handleClick}
-      getTimeTable={this.props.getTimeTable}
-      {...props}
-    />
+    <Branch key={props.id} getTimeTable={this.props.getTimeTable} {...props} />
   )
 
   render() {
-    const { id, name, onClick, isOpen, type } = this.props
+    const { id, name, type } = this.props
     return (
       <li>
-        <a id={id} type={type} role="button" onClick={onClick}>
+        <a id={id} type={type} role="button" onClick={this.handleClick}>
           {name}
         </a>
-        {isOpen && type !== 'group' ? (
-          <ul id={id}>
-            {hierarchy.filter(el => el.parent === id).map(this.elToLink)}
-          </ul>
+        {this.state.isOpen && type !== 'group' ? (
+          <ul id={id}>{this.state.children.map(this.elToLink)}</ul>
         ) : null}
       </li>
     )
