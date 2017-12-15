28de4f9e2189371ea71c5fde18671960e1079fe7
sunalena <33164590+sunalena@users.noreply.github.com>
2017-12-15 12:50:32 +0200

    Update Day.js

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,33 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { CSSTransitionGroup } from 'react-transition-group'
 
 const Lesson = ({ time, discipline, classroom, teacher }) => (
   <tr>
     <td className="ti">{time}</td>
-    <td className="di"><div>{discipline}</div></td>
-    <td className="cl">{classroom}</td>
-    <td className="te"><div>{teacher}</div></td>
+  <td>
+  <div className='cell' >
+    <div className="discip" title={discipline} >{discipline}</div> 
+    <p className="classroom"> Ауд. {classroom} </p>
+    <p className="teacher"> {teacher} </p>
+  </div>
+  </td>
   </tr>
 )
 
 const itemToLesson = ({ id, ...rest }) => <Lesson key={id} {...rest} />
 
-export default ({ id, day, lessons }) => (
-  <table border="1">
-    <caption>
-      <h2> {day}</h2>
-    </caption>
-    <thead>
-      <tr>
-        <th scope="col">Время</th>
-        <th scope="col">
-          Название <br /> пары
-        </th>
-        <th scope="col">
-          № <br />кабинета
-        </th>
-        <th scope="col">Преподаватель</th>
-      </tr>
-    </thead>
-    <tbody>{lessons.map(itemToLesson)}</tbody>
+
+const w = (window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth)> 700 ? true : false;
+
+class DropTable extends Component {
+  state = { isOpened: w }
+
+  toggleState = () => {
+    this.setState({ isOpened: !this.state.isOpened })
+  }
+
+  render() {
+    const { id, day, lessons } = this.props
+    return (
+      <div onClick={this.toggleState} className="dropTable">
+          
+          <h2> {day}</h2>
+          
+        <CSSTransitionGroup 
+          transitionName="dropdownMenu"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}>
+
+          {this.state.isOpened && (
+              <table border="1">
+
+              <thead>
+                <tr>
+                  <th scope="col">Время</th>
+                  <th scope="col">
+                    информация
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+    
+    {lessons.map(itemToLesson)}</tbody>
   </table>
-)
+          ) }
+          
+        </CSSTransitionGroup>
+      </div>
+    )
+  }
+}
+export default DropTable
